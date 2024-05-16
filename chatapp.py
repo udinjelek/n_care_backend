d321862468f336b511892234bd10606466e15747
udin jelek <udin@jelek.com>
2024-05-17 00:27:31 +0700

    try evenlet wsgi

--- chatapp.py
+++ chatapp.py
@@ -4,9 +4,13 @@
 #     app.run(debug=True, port=5001)
 
 from app import app, socketio
+import eventlet
+from eventlet import wsgi
 
 if __name__ == "__main__":
-    socketio.run(app, host='care-chat-api.polaris.my.id')
+    # wsgi.server(eventlet.listen(("127.0.0.1",5001)), app)
+    wsgi.server(eventlet.listen(("care-chat-api.polaris.my.id",8000)), app)
+    # socketio.run(app, host='care-chat-api.polaris.my.id')
     # socketio.run(app, host='0.0.0.0', port=5001)
     # app.run(host='care-chat-api.polaris.my.id', port=5001)
     # app.run(host='0.0.0.0', port=5001)
